0af610bd45fed439bd166efc949f7bd8c1412bf8
Sathya-Narayana <sathya.learningworks@gmail.com>
2024-01-04 12:00:00 +0530

    Implemented linear regression model

--- example.py
+++ example.py
@@ -4,3 +4,4 @@ print('Hello from my MLOps journey!')
 # Initialized MLOps project with Python on 2024-01-01
 # Explored Python for data science on 2024-01-02
 # Started with basic ML algorithms on 2024-01-03
+# Implemented linear regression model on 2024-01-04
