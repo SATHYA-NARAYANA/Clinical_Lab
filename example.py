19bc0b18165a372410d353fc985d8749e169540a
Sathya-Narayana <sathya.learningworks@gmail.com>
2024-01-12 12:00:00 +0530

    Dockerized ML application

--- example.py
+++ example.py
@@ -12,3 +12,4 @@ print('Hello from my MLOps journey!')
 # Explored RNNs and LSTM for time-series data on 2024-01-09
 # Integrated Git for version control on 2024-01-10
 # Setup Jenkins for CI/CD pipeline on 2024-01-11
+# Dockerized ML application on 2024-01-12
