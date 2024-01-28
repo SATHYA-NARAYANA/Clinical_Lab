3eeb10c4e0d9662ea20530cd26e8d9115a2a1c4b
Sathya-Narayana <sathya.learningworks@gmail.com>
2024-01-28 12:00:00 +0530

    Enabled GPU support for DL models

--- example.py
+++ example.py
@@ -28,3 +28,4 @@ print('Hello from my MLOps journey!')
 # Used RedHat Linux for deployment on 2024-01-25
 # Fine-tuned model performance metrics on 2024-01-26
 # Setup alerting in Kubernetes cluster on 2024-01-27
+# Enabled GPU support for DL models on 2024-01-28
