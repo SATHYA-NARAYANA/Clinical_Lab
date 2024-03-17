400ca4a35e646f20b1155c6a5e8cb35f7382c73f
Sathya-Narayana <sathya.learningworks@gmail.com>
2024-03-17 12:00:00 +0530

    Used AWS Lambda for lightweight tasks

--- example.py
+++ example.py
@@ -77,3 +77,4 @@ print('Hello from my MLOps journey!')
 # Created Helm charts for deployment on 2024-03-14
 # Deployed app to AWS EC2 on 2024-03-15
 # Configured S3 for model storage on 2024-03-16
+# Used AWS Lambda for lightweight tasks on 2024-03-17
